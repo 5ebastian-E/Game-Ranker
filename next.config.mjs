33a48a0abadef3c4486e2042ecee0fc4ccec22a7
Sebastian Einert <s.einert@abillify.me>
2024-07-25 11:36:38 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "",
-  assetPrefix: "",
+  basePath: "/Game-Ranker",
+  assetPrefix: "/Game-Ranke/",
 };
 
 export default nextConfig;
